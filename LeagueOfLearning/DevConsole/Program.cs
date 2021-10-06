939607a98f74bdca08d167bafe2d3fa28b2abc18
Brennan Rodriguez <brennan.rodriguez@sciplay.com>
2021-10-06 15:34:00 -0500

    initial commit

---
+++ LeagueOfLearning/DevConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PoniLCU;
+
+namespace DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            LeagueClient leagueClient = new LeagueClient();
+            while (!leagueClient.IsConnected)
+            {
+                continue;
+            }
+            string body = "{\"profileIconId\": "+23+"}";
+            leagueClient.Request("put", "/lol-summoner/v1/current-summoner/icon", body); 
+        }
+    }
+}
