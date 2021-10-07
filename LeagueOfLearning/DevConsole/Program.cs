199935045ab8eef017850899cf9cef49bb151894
Brennan Rodriguez <brennan.rodriguez@sciplay.com>
2021-10-07 11:20:05 -0500

    removed pnilcu

--- LeagueOfLearning/DevConsole/Program.cs
+++ LeagueOfLearning/DevConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using PoniLCU;
 
 namespace DevConsole
 {
@@ -8,13 +7,7 @@ namespace DevConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            LeagueClient leagueClient = new LeagueClient();
-            while (!leagueClient.IsConnected)
-            {
-                continue;
-            }
             string body = "{\"profileIconId\": "+23+"}";
-            leagueClient.Request("put", "/lol-summoner/v1/current-summoner/icon", body); 
         }
     }
 }
