97b6a4f0aad3b0e8cec4c7bf08e9b491bdd9ef25
Brennan Rodriguez <brennan.rodriguez@sciplay.com>
2021-10-07 14:25:29 -0500

    Adding wrappers for riot api

--- LeagueOfLearning/DataScraper/Program.cs
+++ LeagueOfLearning/DataScraper/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Camille.Enums;
+using Camille.RiotGames;
+using KertiRiot;
+using Newtonsoft.Json.Linq;
 
 namespace DataScraper
 {
@@ -6,7 +12,48 @@ namespace DataScraper
     {
         static void Main(string[] args)
         {
+            var stream = new StreamReader("../../../apiKey.txt");
+            var key = stream.ReadToEnd();
+            var riotApi = new RiotApi(key);
             Console.WriteLine("Hello World!");
+            var res = riotApi.CallEndpoint("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
+                                           "Kertaak");
+           var res2 = riotApi.CallEndpoint(string.Format(
+               "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{0}/ids", JObject.Parse(res.ResponseBody)["puuid"]));
+           Console.WriteLine(res2.ResponseBody);
+           var arr = JObject.Parse("{\"matches\":" + res2.ResponseBody + "}");
+           Console.WriteLine(arr.ToString());
+           Console.WriteLine("Len: "+ arr["matches"].Count());
+           var matchID = arr["matches"][0];
+           Console.WriteLine(matchID);
+
+
+
+
+
+           var camApi = RiotGamesApi.NewInstance(key);
+           var summoners = new[]
+           {
+               camApi.SummonerV4().GetBySummonerName(PlatformRoute.NA1, "Kertaak")
+           };
+           foreach (var summoner in summoners)
+           {
+               Console.WriteLine($"{summoner.Name}'s Top 10 Champs:");
+
+               var masteries =
+                   camApi.ChampionMasteryV4().GetAllChampionMasteries(PlatformRoute.NA1, summoner.Id);
+
+               for (var i = 0; i < 10; i++)
+               {
+                   var mastery = masteries[i];
+                   // Get champion for this mastery.
+                   var champ = (Champion) mastery.ChampionId;
+                   // print i, champ id, champ mastery points, and champ level
+                   Console.WriteLine("{0,3}) {1,-16} {2,10:N0} ({3})", i + 1, champ.ToString(),
+                       mastery.ChampionPoints, mastery.ChampionLevel);
+               }
+               Console.WriteLine();
+           } 
         }
     }
 }
---
+++ LeagueOfLearning/KertiRiot/KertiRiot.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Authentication;
+
+namespace KertiRiot
+{
+    public class RiotApi
+    {
+        private HttpClient _httpClient;
+
+        public RiotApi(string apiKey)
+        {
+            _httpClient = new HttpClient(new HttpClientHandler
+            {
+                SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls,
+                ServerCertificateCustomValidationCallback = (a, b, c, d) => true // Auto validate certifs 
+            });
+            _httpClient.DefaultRequestHeaders.Add("X-Riot-Token", apiKey);
+        }
+
+        public struct Response
+        {
+            public Response(HttpResponseHeaders header, string body, HttpStatusCode code)
+            {
+                ResponseHeader = header;
+                ResponseBody = body;
+                ResponseCode = code;
+            }
+
+            public HttpResponseHeaders ResponseHeader;
+            public string ResponseBody;
+            public HttpStatusCode ResponseCode;
+        }
+
+        public Response CallEndpoint(string url)
+        {
+            var result = _httpClient.GetAsync(url);
+            result.Wait();
+            var body = result.Result.Content.ReadAsStringAsync();
+            HttpStatusCode responseCode = result.Result.StatusCode;
+            HttpResponseHeaders header = result.Result.Headers;
+            body.Wait();
+            return new Response(
+                header,
+                body.Result,
+                responseCode
+            );
+        }
+    }
+}
