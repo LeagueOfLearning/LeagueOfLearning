046403c12c452ad79897d969b2187ca5c50653d7
Brennan Rodriguez <brennan.rodriguez@sciplay.com>
2021-10-07 14:29:12 -0500

    Removing KeriRiot

--- LeagueOfLearning/DataScraper/Program.cs
+++ LeagueOfLearning/DataScraper/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using Camille.Enums;
 using Camille.RiotGames;
-using KertiRiot;
-using Newtonsoft.Json.Linq;
 
 namespace DataScraper
 {
@@ -14,23 +11,6 @@ namespace DataScraper
         {
             var stream = new StreamReader("../../../apiKey.txt");
             var key = stream.ReadToEnd();
-            var riotApi = new RiotApi(key);
-            Console.WriteLine("Hello World!");
-            var res = riotApi.CallEndpoint("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
-                                           "Kertaak");
-           var res2 = riotApi.CallEndpoint(string.Format(
-               "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{0}/ids", JObject.Parse(res.ResponseBody)["puuid"]));
-           Console.WriteLine(res2.ResponseBody);
-           var arr = JObject.Parse("{\"matches\":" + res2.ResponseBody + "}");
-           Console.WriteLine(arr.ToString());
-           Console.WriteLine("Len: "+ arr["matches"].Count());
-           var matchID = arr["matches"][0];
-           Console.WriteLine(matchID);
-
-
-
-
-
            var camApi = RiotGamesApi.NewInstance(key);
            var summoners = new[]
            {
